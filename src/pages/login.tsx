60bfe08e1172c8cdc5bd8918ee1b4fae3299fb34
Wiliam Traoré <hi@wil-liam.com>
2022-09-24 18:04:29 +0200

    feat(login): redesign login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -14,7 +14,14 @@ export const Login = () => {
   }
 
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center flex-col gap-10">
+      <div className="w-[55%] h-[60%] bg-primary-500 blur-[200px] rounded-full fixed -top-[20%] -left-[20%]"></div>
+      <div className="w-[60%] h-[15%] bg-accent-500 blur-[200px] rounded-full fixed top-[40%] -right-[40%]"></div>
+      <h1 className="font-semibold text-4xl font-title text-center">
+        Join the best streaming
+        <br />
+        visual editor
+      </h1>
       <Button link={'http://localhost:3333/oauth/twitch/redirect'} iconLeft="twitch-fill" external>
         Login with Twitch
       </Button>
