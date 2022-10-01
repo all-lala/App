e0b1823f47039a7897ad41cd8a70de3a7daa59e8
fabien kerinec <fabien.kerinec@gmail.com>
2022-10-01 16:19:12 +0200

    fix(storybook): fix chat message story

--- src/components/chat/chat-message/chat-message.stories.tsx
+++ src/components/chat/chat-message/chat-message.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { defaultChatTheme } from '../../../utils/chat/default-chat-theme';
 import { ChatMessage } from './chat-message';
 
@@ -7,7 +7,25 @@ export default {
   title: 'Chat/Chat Message',
 } as ComponentMeta<typeof ChatMessage>;
 
-const Template: ComponentStory<typeof ChatMessage> = (args) => <ChatMessage {...args} />;
+const Template: ComponentStory<typeof ChatMessage> = (args) => (
+  <ChatMessage
+    {...args}
+    message={{
+      id: '1',
+      date: new Date(),
+      message: 'Hello world',
+      username: 'John Doe',
+      badges: {
+        admin: true,
+        broadcaster: false,
+        moderator: false,
+        partner: false,
+        vip: false,
+        artist: false,
+      },
+    }}
+  />
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
