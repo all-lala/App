a163a9378d865c97a778cf0f3fcdd482d14ba14d
fabien kerinec <fabien.kerinec@gmail.com>
2022-10-01 16:00:05 +0200

    fix(storybook): fix the popover story

--- src/components/popover/popover.stories.tsx
+++ src/components/popover/popover.stories.tsx
@@ -1,19 +1,27 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Button } from '../button/button';
 import { Popover } from './popover';
+import React from 'react';
 
 export default {
   component: Popover,
   title: 'Popover',
 } as ComponentMeta<typeof Popover>;
 
-const Template: ComponentStory<typeof Popover> = (args) => <Popover {...args} />;
+const Template: ComponentStory<typeof Popover> = (args) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <div style={{ width: 'fit-content' }}>
+      <Popover {...args} open={isOpen} onOpenChange={setIsOpen}>
+        Popover content
+      </Popover>
+    </div>
+  );
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
   trigger: <Button>Trigger popover</Button>,
-  children: 'Popover content',
-  align: 'start',
+  align: 'end',
   side: 'right',
-  open: true,
 };
