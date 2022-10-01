2277917c117e53e315aee0372219a93d3f61cdba
fabien kerinec <fabien.kerinec@gmail.com>
2022-10-01 15:37:42 +0200

    fix(storybook): fix the popover-navigation

--- src/components/popover/navigation/popover-navigation.stories.tsx
+++ src/components/popover/navigation/popover-navigation.stories.tsx
@@ -1,9 +1,22 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { PopoverNavigation } from './popover-navigation';
 
 export default {
   component: PopoverNavigation,
   title: 'PopoverNavigation',
+  args: {
+    links: [
+      {
+        title: 'Show',
+        link: '/',
+      },
+      {
+        icon: 'home-line',
+        title: 'Create',
+        link: '/create',
+      },
+    ],
+  },
 } as ComponentMeta<typeof PopoverNavigation>;
 
 const Template: ComponentStory<typeof PopoverNavigation> = (args) => (
