996409293c28b69340decff9ae1e4c60f3378716
fabien kerinec <fabien.kerinec@gmail.com>
2022-10-01 16:22:14 +0200

    fix(storybook): fix text-style story

--- src/components/forms/text-style/text-style.stories.tsx
+++ src/components/forms/text-style/text-style.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { TextStyle } from './text-style';
 
 export default {
@@ -9,4 +9,17 @@ export default {
 const Template: ComponentStory<typeof TextStyle> = (args) => <TextStyle {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  settings: {
+    fontFamily: 'Rubik',
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#000000',
+    textAlign: 'left',
+    textDecoration: 'none',
+    fontStyle: 'normal',
+    letterSpacing: 0,
+    lineHeight: 100,
+    textShadow: { shadowOffsetX: 0, shadowOffsetY: 0, shadowBlur: 0, shadowColor: '#000000' },
+  },
+};
