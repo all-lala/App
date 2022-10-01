012fce92ad32e89b4dbdcee0c01144bcffe770d0
fabien kerinec <fabien.kerinec@gmail.com>
2022-10-01 15:30:35 +0200

    feat(storybook): add a story with all the icon visible directly

--- src/components/icon/icon.stories.tsx
+++ src/components/icon/icon.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Icon } from './icon';
+import { Icon, IconSVG } from './icon';
 
 export default {
   component: Icon,
@@ -12,3 +12,18 @@ export const Primary = Template.bind({});
 Primary.args = {
   name: 'community-line',
 };
+
+export const AllIcon: ComponentStory<typeof Icon> = () => (
+  <>
+    {Object.values(IconSVG)
+      .sort((a, b) => a.localeCompare(b))
+      .map((iconName) => (
+        <span
+          key={iconName}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px' }}
+        >
+          {iconName.charAt(0).toUpperCase() + iconName.slice(1)}: <Icon svg={iconName} />
+        </span>
+      ))}
+  </>
+);
