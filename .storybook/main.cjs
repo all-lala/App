ceb64ad52bf1f62406261ae4ee5629e72fbcbde0
Romain Lanz <romain.lanz+github@pm.me>
2022-10-03 22:00:25 +0200

    chore: ensure storybook use vite aliases when building

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,19 +1,26 @@
+const tsconfigPaths = require('vite-tsconfig-paths').default;
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: async (config) => {
+    config.plugins.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths()
+    );
+
+    return config;
+  },
+};
