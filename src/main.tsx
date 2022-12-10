342b87e40ed53e0b5a9a1430ddc1054c6c9a2586
Romain Lanz <romain.lanz+github@pm.me>
2022-12-10 17:39:33 +0100

    feat: properly define if we should retry the query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,33 @@
 import './main.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from '~/app';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        if (error instanceof AxiosError && error.response) {
+          const status = error.response.status;
+
+          if ([400, 401, 404].includes(status)) {
+            return false;
+          }
+
+          if ([500, 503, 504].includes(status) && failureCount <= 5) {
+            return true;
+          }
+        }
+
+        return false;
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
